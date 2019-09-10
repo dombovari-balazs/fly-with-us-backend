74937fc3d1e31b55a7da97b8c57e85248fc14205
LibLev <libardi.levente@gmail.com>
2019-09-10 13:13:43 +0200

    Initial commit

---
+++ src/main/java/com/codecool/fwu_backend/FwuBackendApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.fwu_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FwuBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FwuBackendApplication.class, args);
+    }
+
+}
