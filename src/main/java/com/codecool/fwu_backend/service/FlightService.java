e4f2d0d454c54fc505d4bb658ba3edd5f04d3ee8
Dombi <dbalazs1219@gmail.com>
2019-10-21 11:20:26 +0200

    FlightDto in controller added
    findAll()

--- src/main/java/com/codecool/fwu_backend/service/FlightService.java
+++ src/main/java/com/codecool/fwu_backend/service/FlightService.java
@@ -53,8 +53,11 @@ public class FlightService {
 
     }
 
-    public List<Flight> findAllFlight() {
-        return flightStorage.findAll();
+    public List<FlightDto> findAllFlight() {
+        return flightStorage.findAll()
+                .stream()
+                .map(FlightDto::new)
+                .collect(Collectors.toList());
     }
 
     public List<Airport> findAllAirport() {
